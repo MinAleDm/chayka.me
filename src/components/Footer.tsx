baaa71ffc383a66a1afbc5a9ab85736a29bc8fa1
yachayka <petrushenko184@gmail.com>
2025-10-14 10:52:49 +0300

    PS#1 // Сброс наработок и начало с 0
    Полная очистка репозитория и начало разработки с 0

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full py-4 border-t border-gray-700 text-center text-gray-500 text-sm">
-      © 2025 MinAleDm. Все права защищены.
-    </footer>
-  );
-}
